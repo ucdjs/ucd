a8d1d163f76d4b5ecbf377d647313d976e409a25
Lucas <lucasnrgaard@gmail.com>
2025-06-01 15:17:49 +0200

    export UnicodeVersionFile

--- packages/ucd-store/src/index.ts
+++ packages/ucd-store/src/index.ts
@@ -1,4 +1,17 @@
-export { LocalUCDStore, type LocalUCDStoreOptions } from "./local";
-export type { UCDStoreRootSchema, UCDStoreVersionSchema } from "./metadata";
-export { RemoteUCDStore, type RemoteUCDStoreOptions } from "./remote";
-export { createUCDStore, type CreateUCDStoreOptions } from "./store";
+export {
+  LocalUCDStore,
+  type LocalUCDStoreOptions,
+} from "./local";
+export type {
+  UCDStoreRootSchema,
+  UCDStoreVersionSchema,
+} from "./metadata";
+export {
+  RemoteUCDStore,
+  type RemoteUCDStoreOptions,
+} from "./remote";
+export {
+  createUCDStore,
+  type CreateUCDStoreOptions,
+  type UnicodeVersionFile,
+} from "./store";
