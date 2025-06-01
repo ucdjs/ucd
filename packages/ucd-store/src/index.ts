2dbbe5f05a2c8eb797a6f220c456646078f536c1
Lucas <lucasnrgaard@gmail.com>
2025-06-01 08:52:36 +0200

    more work

--- packages/ucd-store/src/index.ts
+++ packages/ucd-store/src/index.ts
@@ -1,2 +1,4 @@
+export { LocalUCDStore, type LocalUCDStoreOptions } from "./local";
 export type { UCDStoreRootSchema, UCDStoreVersionSchema } from "./metadata";
+export { RemoteUCDStore, type RemoteUCDStoreOptions } from "./remote";
 export { createUCDStore, type CreateUCDStoreOptions } from "./store";
