ebd055e7718c93bad685543a8a752d41e4570d46
Lucas Nørgård <lucasnrgaard@gmail.com>
2025-06-07 08:09:48 +0200

    feat: add playground scripts for local and remote UCDStore

---
+++ packages/ucd-store/playgrounds/remote-playground.ts
@@ -0,0 +1,31 @@
+/* eslint-disable antfu/no-top-level-await */
+/* eslint-disable no-console */
+import assert from "node:assert";
+import { RemoteUCDStore } from "../src/remote";
+import { createUCDStore } from "../src/store";
+
+const store = await createUCDStore("remote");
+assert(store instanceof RemoteUCDStore, "store should be an instance of RemoteUCDStore");
+
+assert(store.baseUrl != null, "store baseUrl should not be null");
+assert(store.proxyUrl != null, "store proxyUrl should not be null");
+assert(store.versions.length > 0, "store should have versions");
+
+console.log("[remote-store]", store.versions);
+
+console.log("[remote-store] hasVersion: 15.0.0", await store.hasVersion("15.0.0"));
+console.log("[remote-store] hasVersion: 99.0.0", await store.hasVersion("99.0.0"));
+
+const filePathsForV16 = await store.getFilePaths("16.0.0");
+assert(filePathsForV16.length > 0, "file paths for version 16.0.0 should not be empty");
+console.log("[remote-store] file paths for 16.0.0:", filePathsForV16);
+
+const fileContent = await store.getFile("16.0.0", "UnicodeData.txt");
+console.log("[remote-store] file content for 16.0.0/UnicodeData.txt:", fileContent.slice(0, 100), "...");
+
+assert(fileContent.length > 0, "file content should not be empty");
+
+assert(store.fileCache.size > 0, "file cache should not be empty");
+assert(store.fileCache.has("16.0.0/UnicodeData.txt"), "file cache should contain the file for 16.0.0/UnicodeData.txt");
+
+console.log("[remote-store]", store);
