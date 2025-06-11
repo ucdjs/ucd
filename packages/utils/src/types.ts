b7e05f614b82929fce7b08eb586d0d379dcfdd4a
Lucas Nørgård <lucasnrgaard@gmail.com>
2025-06-11 21:02:49 +0200

    feat: enhance mirrorUCDFiles to handle errors and return located files

--- packages/utils/src/types.ts
+++ packages/utils/src/types.ts
@@ -1,5 +1,8 @@
 // ONLY STOW TYPES IN HERE WHEN THEY CAN'T BE COLOCATED WITH THE CODE THAT USES THEM
 
+import type { MakeDirectoryOptions, Mode } from "node:fs";
+
 export interface FSAdapter {
   readFile: (path: string) => Promise<string>;
+  mkdir: (path: string, options?: Mode | MakeDirectoryOptions | null) => Promise<string | undefined>;
 }
