6a0edfa9bd2ea7192c84e8f983d0f8563b4a7eaf
Lucas <lucasnrgaard@gmail.com>
2025-06-14 08:31:57 +0200

    refactor: remove deprecated FsInterface tests and related code; implement repairUCDFiles functionality with improved error handling and file downloading logic

--- packages/utils/src/ucd-files.ts
+++ packages/utils/src/ucd-files.ts
@@ -7,5 +7,5 @@ export { flattenFilePaths } from "./ucd-files/helpers";
 export type { DownloadError, MirrorOptions, MirrorResult } from "./ucd-files/mirror";
 export { mirrorUCDFiles } from "./ucd-files/mirror";
 
-export { validateUCDFiles } from "./ucd-files/validate";
-export type { ValidateUCDFilesOptions, ValidateUCDFilesResult } from "./ucd-files/validate";
+export { repairUCDFiles, validateUCDFiles } from "./ucd-files/validate";
+export type { RepairUCDFilesOptions, RepairUCDFilesResult, ValidateUCDFilesOptions, ValidateUCDFilesResult } from "./ucd-files/validate";
