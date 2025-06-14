825a1371c1f6c7edd8e9319057eae5a15f91431d
Lucas <lucasnrgaard@gmail.com>
2025-06-14 07:36:01 +0200

    refactor: migrate ucd-store to use utils

--- packages/utils/src/ucd-files.ts
+++ packages/utils/src/ucd-files.ts
@@ -2,8 +2,10 @@ export {
   createDefaultFSAdapter,
 } from "./ucd-files/fs-adapter";
 
-export { mirrorUCDFiles } from "./ucd-files/mirror";
+export { flattenFilePaths } from "./ucd-files/helpers";
+
 export type { DownloadError, MirrorOptions, MirrorResult } from "./ucd-files/mirror";
+export { mirrorUCDFiles } from "./ucd-files/mirror";
 
 export { validateUCDFiles } from "./ucd-files/validate";
 export type { ValidateUCDFilesOptions, ValidateUCDFilesResult } from "./ucd-files/validate";
