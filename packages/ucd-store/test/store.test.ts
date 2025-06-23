0a17656b267f4f971df46817dd229c148d3ded47
Lucas Nørgård <lucasnrgaard@gmail.com>
2025-06-23 06:04:10 +0200

    chore: lint

--- packages/ucd-store/test/store.test.ts
+++ packages/ucd-store/test/store.test.ts
@@ -1,9 +1,4 @@
 import { describe, expect, it } from "vitest";
-import {
-  createUCDStore,
-  UCDStore,
-  type UCDStoreOptions,
-} from "../src/store";
 
 describe("todo", () => {
   it("todo", () => {
