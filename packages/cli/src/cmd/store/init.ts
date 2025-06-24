4c824601f7bde6d24ad0afb5290023d39fd7227d
Lucas <lucasnrgaard@gmail.com>
2025-06-24 05:39:22 +0200

    refactor(store): replace `createUCDStore` with specific local and remote store creation functions
    * Updated `clean`, `init`, `repair`, and `status` commands to use `createLocalUCDStore` and `createRemoteUCDStore`.
    * Improved clarity and specificity in store creation logic.
    * Adjusted `AnalyzeResult` and `CleanResult` types for better structure and clarity.

--- packages/cli/src/cmd/store/init.ts
+++ packages/cli/src/cmd/store/init.ts
@@ -1,6 +1,6 @@
 import type { Prettify } from "@luxass/utils";
 import type { CLIArguments } from "../../cli-utils";
-import { createUCDStore, type UCDStore } from "@ucdjs/ucd-store";
+import { createLocalUCDStore, createRemoteUCDStore, type UCDStore } from "@ucdjs/ucd-store";
 import { printHelp } from "../../cli-utils";
 import { type CLIStoreCmdSharedFlags, SHARED_FLAGS } from "./_shared";
 
@@ -47,17 +47,17 @@ export async function runInitStore({ flags, versions }: CLIStoreInitCmdOptions) 
 
   let store: UCDStore | null = null;
   if (remote) {
-    store = await createUCDStore("remote", {
+    store = await createRemoteUCDStore({
       baseUrl,
       proxyUrl,
-      filters: patterns,
+      globalFilters: patterns,
     });
   } else {
-    store = await createUCDStore("local", {
+    store = await createLocalUCDStore({
       basePath: storeDir,
       baseUrl,
       proxyUrl,
-      filters: patterns,
+      globalFilters: patterns,
     });
   }
 
