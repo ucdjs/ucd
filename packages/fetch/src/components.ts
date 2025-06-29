e6c761e4cca8e1fe276f40880ba7856e55967e86
Lucas <lucasnrgaard@gmail.com>
2025-06-29 15:23:12 +0200

    chore: fix tests

--- packages/fetch/src/components.ts
+++ packages/fetch/src/components.ts
@@ -9,6 +9,7 @@ import type { components } from "./.generated/api";
 export type UnicodeVersion = components["schemas"]["UnicodeVersion"];
 export type UnicodeVersions = components["schemas"]["UnicodeVersions"];
 export type ApiError = components["schemas"]["ApiError"];
+export type UnicodeVersionMappings = components["schemas"]["UnicodeVersionMappings"];
 export type ProxyDirectoryResponse = components["schemas"]["ProxyDirectoryResponse"];
 export type ProxyFileResponse = components["schemas"]["ProxyFileResponse"];
 export type ProxyResponse = components["schemas"]["ProxyResponse"];
