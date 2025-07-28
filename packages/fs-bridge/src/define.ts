6763abcd8202777afdfb418e185f1b548e9c713c
Lucas <lucasnrgaard@gmail.com>
2025-07-28 12:49:55 +0200

    feat(fs-bridge): add debug symbol to file system bridge
    * Introduced `__BRIDGE_DEBUG_SYMBOL__DO_NOT_USE_OR_YOU_WILL_BE_FIRED__` for debugging purposes.
    * Updated `defineFileSystemBridge` to attach capabilities to the debug symbol.

--- packages/fs-bridge/src/define.ts
+++ packages/fs-bridge/src/define.ts
@@ -14,6 +14,27 @@ const DEFAULT_SUPPORTED_CAPABILITIES: FileSystemBridgeCapabilities = {
   rm: true,
 };
 
+/**
+ * Internal debug symbol used to attach capability information to file system bridge instances.
+ *
+ * @internal
+ * @remarks
+ * This symbol is used internally by the fs-bridge package to store debugging information
+ * about bridge capabilities. It should never be used by external code as it may change
+ * or be removed without notice.
+ *
+ * @warning
+ * The name explicitly indicates this is for internal use only. Using this symbol in
+ * external code will likely break your application when the package is updated.
+ *
+ * @example
+ * ```typescript
+ * // DON'T DO THIS - Internal use only
+ * const capabilities = bridge[__BRIDGE_DEBUG_SYMBOL__DO_NOT_USE_OR_YOU_WILL_BE_FIRED__];
+ * ```
+ */
+export const __BRIDGE_DEBUG_SYMBOL__DO_NOT_USE_OR_YOU_WILL_BE_FIRED__: unique symbol = Symbol.for("ucdjs.fs-bridge.debug");
+
 export function defineFileSystemBridge<
   TOptionsSchema extends z.ZodType,
   TState extends Record<string, unknown> = Record<string, unknown>,
@@ -33,10 +54,14 @@ export function defineFileSystemBridge<
 
     const { capabilities = DEFAULT_SUPPORTED_CAPABILITIES, state } = fsBridge;
 
-    return fsBridge.setup({
+    const bridge = fsBridge.setup({
       options,
       state: state ?? {} as TState,
       capabilities,
     });
+
+    (bridge as any)[__BRIDGE_DEBUG_SYMBOL__DO_NOT_USE_OR_YOU_WILL_BE_FIRED__] = capabilities;
+
+    return bridge;
   };
 }
