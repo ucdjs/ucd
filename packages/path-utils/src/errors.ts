4e17d21c4581aa65290dc855cc4752571f17c4bd
Lucas <lucasnrgaard@gmail.com>
2025-09-07 06:11:28 +0200

    feat(path-utils): add custom error classes for path traversal and Windows path issues
    * Introduced `PathTraversalError`, `WindowsDriveMismatchError`, `WindowsPathTypeMismatchError`, and `WindowsUNCShareMismatchError` classes.
    * Each class extends `PathUtilsBaseError` and provides specific error messages for various path-related issues.
    * Enhances error handling capabilities in path utilities.

--- packages/path-utils/src/errors.ts
+++ packages/path-utils/src/errors.ts
@@ -11,3 +11,51 @@ export class MaximumDecodingIterationsExceededError extends PathUtilsBaseError {
     this.name = "MaximumDecodingIterationsExceededError";
   }
 }
+
+export class PathTraversalError extends PathUtilsBaseError {
+  public readonly accessedPath: string;
+  public readonly basePath: string;
+
+  constructor(basePath: string, accessedPath: string) {
+    super(`Path traversal detected: attempted to access '${accessedPath}' which is outside the allowed base path '${basePath}'`);
+    this.name = "PathTraversalError";
+    this.basePath = basePath;
+    this.accessedPath = accessedPath;
+  }
+}
+
+export class WindowsDriveMismatchError extends PathUtilsBaseError {
+  public readonly accessedDrive: string;
+  public readonly baseDrive: string;
+
+  constructor(baseDrive: string, accessedDrive: string) {
+    super(`Drive letter mismatch detected: attempted to access '${accessedDrive}' which is on a different drive than the allowed base drive '${baseDrive}'`);
+    this.name = "WindowsDriveMismatchError";
+    this.baseDrive = baseDrive;
+    this.accessedDrive = accessedDrive;
+  }
+}
+
+export class WindowsPathTypeMismatchError extends PathUtilsBaseError {
+  public readonly basePathType: string;
+  public readonly inputPathType: string;
+
+  constructor(basePathType: string, inputPathType: string) {
+    super(`Cannot combine ${inputPathType} path with ${basePathType} base path on Windows`);
+    this.name = "WindowsPathTypeMismatchError";
+    this.basePathType = basePathType;
+    this.inputPathType = inputPathType;
+  }
+}
+
+export class WindowsUNCShareMismatchError extends PathUtilsBaseError {
+  public readonly baseShare: string;
+  public readonly inputShare: string;
+
+  constructor(baseShare: string, inputShare: string) {
+    super(`Different UNC shares not allowed: base share '${baseShare}' differs from input share '${inputShare}'`);
+    this.name = "WindowsUNCShareMismatchError";
+    this.baseShare = baseShare;
+    this.inputShare = inputShare;
+  }
+}
