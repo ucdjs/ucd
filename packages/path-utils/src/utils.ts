a6cebc3eb7c45eb69e6bfd04aa98e10f44b98198
Lucas <lucasnrgaard@gmail.com>
2025-09-07 10:50:15 +0200

    feat(path-utils): export osPlatform for platform detection
    * Added `osPlatform` to export for better platform detection.
    * This allows users to easily access the current operating system platform.

--- packages/path-utils/src/utils.ts
+++ packages/path-utils/src/utils.ts
@@ -6,8 +6,13 @@ export const isCaseSensitive: boolean = /* @__PURE__ */ (() => "process" in glob
   && typeof globalThis.process.platform === "string"
   && (globalThis.process.platform !== "win32" && globalThis.process.platform !== "darwin"))();
 
-export const isWindows: boolean = /* @__PURE__ */ (() => "process" in globalThis
-  && typeof globalThis.process === "object"
-  && "platform" in globalThis.process
-  && typeof globalThis.process.platform === "string"
-  && globalThis.process.platform === "win32")();
+export const osPlatform: NodeJS.Platform | null = /* @__PURE__ */ (() => {
+  if ("process" in globalThis
+    && typeof globalThis.process === "object"
+    && "platform" in globalThis.process
+    && typeof globalThis.process.platform === "string") {
+    return globalThis.process.platform;
+  }
+
+  return null;
+})();
