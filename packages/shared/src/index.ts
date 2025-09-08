2fe32717deb4023e95d3ba67be75046375c57006
Lucas <lucasnrgaard@gmail.com>
2025-09-08 17:51:09 +0200

    chore: add debug package and types
    * Added `debug` as a production dependency.
    * Included `@types/debug` in devDependencies for TypeScript support.
    * Introduced `createDebugger` function in `debugger.ts` for logging with namespaces.
    * Updated exports in `index.ts` to include the new `debugger` module.

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,7 +1,8 @@
+export * from "./debugger";
 export { createPathFilter, PRECONFIGURED_FILTERS } from "./filter";
 export type { FilterOptions, PathFilter } from "./filter";
 export { flattenFilePaths } from "./flatten";
-export { safeJsonParse } from "./json";
 
+export { safeJsonParse } from "./json";
 export * from "./promise-concurrency";
 export * from "./try-catch";
