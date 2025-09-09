0e487ba268d1f5dbd038594d99d6aae7d2777d0c
Lucas <lucasnrgaard@gmail.com>
2025-09-09 20:02:11 +0200

    chore: format

--- packages/shared/test/flatten.test.ts
+++ packages/shared/test/flatten.test.ts
@@ -66,7 +66,6 @@ describe("flattenFilePaths", () => {
   });
 
   it("should handle deeply nested structures", () => {
-    ;
     const result = flattenFilePaths([
       {
         type: "directory",
