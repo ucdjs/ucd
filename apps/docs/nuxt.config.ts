fcbf84b112cc78fbab0f84406a0bff8f759207b4
Lucas <lucasnrgaard@gmail.com>
2025-10-03 06:19:51 +0200

    fix(docs): correct import statement and formatting
    * Updated the import statement in `eslint.config.mjs` to use consistent double quotes.
    * Added missing commas in `nuxt.config.ts` for proper syntax.
    * Organized the `eslint` configuration for better readability.

--- apps/docs/nuxt.config.ts
+++ apps/docs/nuxt.config.ts
@@ -4,12 +4,33 @@ export default defineNuxtConfig({
     "@nuxt/eslint",
     "@nuxt/content",
     "@nuxt/ui",
-    "@nuxt/test-utils"
+    "@nuxt/test-utils",
   ],
+  compatibilityDate: "2025-07-18",
   nitro: {
     preset: "cloudflare_module",
     cloudflare: {
       deployConfig: false,
     },
-  }
-})
+  },
+  eslint: {
+    config: {
+      stylistic: {
+        quotes: "double",
+        semi: true,
+      },
+      devtools: {
+        enabled: true,
+      },
+    },
+  },
+  llms: {
+    domain: "https://docs.ucdjs.dev",
+    title: "UCD.js",
+    description: "Write beautiful docs with Markdown.",
+    full: {
+      title: "UCD.js",
+      description: "Write beautiful docs with Markdown.",
+    },
+  },
+});
