0ffc6768d3360b1e9f0507c70843eb9b58027a1f
Lucas <lucasnrgaard@gmail.com>
2025-10-04 10:57:28 +0200

    feat(test-utils): add mockFetch function for MSW integration
    * Introduced `mockFetch` to facilitate fetching in environments where MSW is set up.
    * Throws an error if used outside of the appropriate context, enhancing error handling.

--- packages/test-utils/src/msw.ts
+++ packages/test-utils/src/msw.ts
@@ -1,4 +1,5 @@
 import type { SetupServerApi } from "msw/node";
+import { createMockFetch } from "@luxass/msw-utils";
 
 declare global {
   // eslint-disable-next-line vars-on-top
@@ -7,3 +8,11 @@ declare global {
 
 export { createMockFetch } from "@luxass/msw-utils";
 export { http, HttpResponse } from "msw";
+
+export const mockFetch = globalThis.__ucd_msw_server
+  ? createMockFetch({ mswServer: globalThis.__ucd_msw_server })
+  : () => {
+      throw new Error(
+        "mockFetch can only be used in an environment where MSW is set up, such as in Vitest with the @ucdjs/test-utils/msw/vitest-setup module imported.",
+      );
+    };
