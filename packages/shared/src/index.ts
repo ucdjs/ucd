b1034af8b2ee9316f26a70da89c2ed3feb0560f8
Lucas <lucasnrgaard@gmail.com>
2025-10-13 12:37:24 +0200

    feat(shared): export custom fetch

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -2,16 +2,19 @@ export * from "./async/promise-concurrency";
 export * from "./async/try-catch";
 export * from "./debugger";
 
+export { customFetch } from "./fetch/fetch";
+
 export {
   createPathFilter,
   filterTreeStructure,
   PRECONFIGURED_FILTERS,
 } from "./filter";
+
 export type {
   PathFilter,
   PathFilterOptions,
   TreeEntry,
 } from "./filter";
-
 export { flattenFilePaths } from "./flatten";
+
 export { safeJsonParse } from "./json";
