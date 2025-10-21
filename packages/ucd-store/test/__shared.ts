c37b22ac58dd0fb672fa32f0accc5ae451448c47
Lucas <lucasnrgaard@gmail.com>
2025-10-21 04:42:33 +0200

    chore: lint

--- packages/ucd-store/test/__shared.ts
+++ packages/ucd-store/test/__shared.ts
@@ -1,9 +1,4 @@
-import type { FSEntry } from "@ucdjs/fs-bridge";
-import type Dirent from "memfs/lib/node/Dirent";
-import { dirname, join, relative } from "node:path";
-import { trimTrailingSlash } from "@luxass/utils";
 import { defineFileSystemBridge } from "@ucdjs/fs-bridge";
-import { memfs } from "memfs";
 
 export const createReadOnlyMockFS = defineFileSystemBridge({
   meta: {
