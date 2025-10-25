129e24a77dea2066202722bb89f3a782bc367fd8
Lucas <lucasnrgaard@gmail.com>
2025-10-25 13:02:59 +0200

    refactor(test-utils): reorganize types and handlers
    * Moved route handler definitions to a new `types.ts` file for better organization.
    * Imported `MOCK_ROUTES` from the new `handlers/index.ts` to streamline the mock store API.
    * Removed unused type definitions to simplify the codebase.

---
+++ packages/test-utils/src/mock-store/types.ts
@@ -0,0 +1,97 @@
+import type { MockFetchFn } from "@luxass/msw-utils";
+import type { DefaultBodyType, HttpResponseResolver, PathParams } from "msw";
+import type { paths } from "../.generated/api";
+import type { MOCK_ROUTES } from "./handlers";
+
+interface ContentTypeToType {
+  "application/json": any;
+  "text/plain": string;
+  "application/octet-stream": Blob | File | ArrayBuffer | Uint8Array;
+}
+
+export type EndpointWithGet = {
+  [K in keyof paths]: paths[K] extends { get: any } ? K : never;
+}[keyof paths];
+
+export type TypedResponseResolver<
+  Params extends PathParams<keyof Params> = PathParams,
+  Response extends DefaultBodyType = DefaultBodyType,
+> = HttpResponseResolver<Params, DefaultBodyType, Response>;
+
+type ExtractPathParams<Path extends string> = Path extends `${infer _Start}/{${infer Param}}${infer Rest}`
+  ? PathParams<Param> & ExtractPathParams<Rest>
+  // eslint-disable-next-line ts/no-empty-object-type
+  : {};
+
+type InferContentTypes<Content> = Content extends Record<string, any>
+  ? {
+      [K in keyof Content]: K extends keyof ContentTypeToType
+        ? Content[K] extends ContentTypeToType[K]
+          ? Content[K]
+          : ContentTypeToType[K]
+        : Content[K];
+    }[keyof Content]
+  : never;
+
+type InferResponsesByEndpoint<Endpoint extends EndpointWithGet>
+  = paths[Endpoint]["get"]["responses"] extends infer Responses
+    ? {
+        [StatusCode in keyof Responses]: Responses[StatusCode] extends { content: infer Content }
+          ? InferContentTypes<Content>
+          : never;
+      }[keyof Responses]
+    : never;
+
+export type InferEndpointConfig<
+  Routes extends readonly RouteHandlerDefinition<any>[],
+> = {
+  [K in Routes[number]["endpoint"]]: Extract<Routes[number], {
+    endpoint: K;
+  }> extends RouteHandlerDefinition<infer E>
+    ? InferResponsesByEndpoint<E> | TypedResponseResolver<ExtractPathParams<E>, InferResponsesByEndpoint<E>> | true
+    : never;
+};
+
+interface MockRouteHandlerContext<Endpoint extends EndpointWithGet> {
+  url: string;
+  providedResponse: InferResponsesByEndpoint<Endpoint> | TypedResponseResolver<ExtractPathParams<Endpoint>, InferResponsesByEndpoint<Endpoint>> | true;
+  mockFetch: MockFetchFn;
+  versions: string[];
+  shouldUseDefaultValue: boolean;
+}
+
+export interface RouteHandlerDefinition<Endpoint extends EndpointWithGet> {
+  endpoint: Endpoint;
+  setup: (context: MockRouteHandlerContext<Endpoint>) => void;
+}
+
+type DerivedEndpointConfig = InferEndpointConfig<typeof MOCK_ROUTES>;
+
+type DerivedResponses = Partial<{
+  [K in keyof DerivedEndpointConfig]: false | DerivedEndpointConfig[K];
+}>;
+
+export interface MockStoreConfig {
+  /**
+   * The base URL for the store.
+   *
+   * @default "https://api.ucdjs.dev"
+   */
+  baseUrl?: string;
+
+  /**
+   * The responses to mock for the store endpoints.
+   *
+   * NOTE:
+   * If the value provided is `true`, then a default handler will be used.
+   * If the value is `false`, then no handler will be used.
+   * If the value provided is a specific response, then that response will be used.
+   */
+  responses?: DerivedResponses;
+
+  /**
+   * The versions to use for placeholders
+   * @default ["16.0.0","15.1.0","15.0.0"]
+   */
+  versions?: string[];
+}
