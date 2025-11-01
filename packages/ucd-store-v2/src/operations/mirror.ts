057fb8df16606b6be85d848383c5f544ebf7a1c5
Lucas <lucasnrgaard@gmail.com>
2025-11-01 16:09:04 +0100

    Potential fix for pull request finding 'Comparison between inconvertible types'

--- packages/ucd-store-v2/src/operations/mirror.ts
+++ packages/ucd-store-v2/src/operations/mirror.ts
@@ -353,7 +353,6 @@ export async function mirror(
           content = data;
         } else if (
           typeof data === "object"
-          && data != null
           && !Array.isArray(data)
           && Object.prototype.toString.call(data) === "[object Object]"
           && contentType === "application/json"
