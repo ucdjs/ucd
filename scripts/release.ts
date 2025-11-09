aa33772f19854ae6d2eddad771cf570246025191
Lucas <lucasnrgaard@gmail.com>
2025-11-09 05:54:05 +0100

    chore: update release-scripts

--- scripts/release.ts
+++ scripts/release.ts
@@ -1,13 +1,17 @@
 import { release } from '@ucdjs/release-scripts'
 
+const workspaceRoot = new URL('../', import.meta.url);
+
 release({
   repo: 'ucdjs/ucd',
+  workspaceRoot: workspaceRoot.pathname,
   packages: {
     excludePrivate: true,
     excluded: [
       "vscode-ucd"
     ]
   },
+  safeguards: true,
   prompts: {
     packages: process.env.CI == null,
     versions: process.env.CI == null,
