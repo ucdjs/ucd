8bba6148733eceb0e45f247960a241294752c455
Lucas <lucasnrgaard@gmail.com>
2025-11-17 05:23:59 +0100

    chore: add verification step to release workflow
    - Introduced a new job `verify` in the GitHub Actions workflow to validate the release process.
    - Added a new script `release:verify` in `package.json` to execute the verification logic.
    - Created `release-verify.ts` to handle the verification logic using `@ucdjs/release-scripts`.

---
+++ scripts/release-verify.ts
@@ -0,0 +1,19 @@
+import { verify } from "@ucdjs/release-scripts"
+
+const workspaceRoot = new URL("../", import.meta.url);
+
+verify({
+  repo: "ucdjs/ucd",
+  workspaceRoot: workspaceRoot.pathname,
+  packages: {
+    excludePrivate: true,
+    exclude: [
+      "vscode-ucd"
+    ]
+  },
+  prompts: {
+    packages: process.env.CI == null,
+    versions: process.env.CI == null,
+  },
+  githubToken: process.env.UCDJS_RELEASE_TOKEN!,
+})
