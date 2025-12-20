410c31a4632be4be7d6a313c8502f10538b5d4e4
Lucas <lucasnrgaard@gmail.com>
2025-12-20 16:56:28 +0100

    fix(test-utils): update well-known store manifest import to store version manifest
    Replaced the import of `wellKnownStoreManifest` with `wellKnownStoreVersionManifest` in the mock store handlers. This change ensures the correct versioning is referenced, aligning with recent updates in the UCD store structure.

--- packages/test-utils/src/mock-store/handlers/index.ts
+++ packages/test-utils/src/mock-store/handlers/index.ts
@@ -1,7 +1,7 @@
 import { fileTreeRoute } from "./file-tree";
 import { filesRoute } from "./files";
 import { versionsRoute } from "./versions";
-import { wellKnownConfig, wellKnownStoreManifest } from "./well-known";
+import { wellKnownConfig, wellKnownStoreVersionManifest } from "./well-known";
 
 // MSW runs these handlers in left-to-right,
 // but since we are using `.use`, then the order is actually reversed.
@@ -9,6 +9,6 @@ export const MOCK_ROUTES = [
   filesRoute,
   fileTreeRoute,
   wellKnownConfig,
-  wellKnownStoreManifest,
+  wellKnownStoreVersionManifest,
   versionsRoute,
 ] as const;
