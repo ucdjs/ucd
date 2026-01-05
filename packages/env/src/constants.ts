fc681fc0e2a4287dfc67229d536cff36b19617ab
Lucas <lucasnrgaard@gmail.com>
2026-01-05 06:59:11 +0100

    feat(env): add new headers for UCD statistics

--- packages/env/src/constants.ts
+++ packages/env/src/constants.ts
@@ -6,10 +6,15 @@
  *
  * @example
  * ```typescript
- * import { UCD_STAT_TYPE_HEADER } from "@ucdjs/env";
+ * import { UCD_STAT_TYPE_HEADER, UCD_STAT_SIZE_HEADER } from "@ucdjs/env";
  *
  * headers[UCD_STAT_TYPE_HEADER] = "file";
+ * headers[UCD_STAT_SIZE_HEADER] = "1024";
  * ```
  */
 
 export const UCD_STAT_TYPE_HEADER = "X-UCD-Stat-Type";
+export const UCD_STAT_SIZE_HEADER = "X-UCD-Stat-Size";
+export const UCD_STAT_CHILDREN_HEADER = "X-UCD-Stat-Children";
+export const UCD_STAT_CHILDREN_FILES_HEADER = "X-UCD-Stat-Children-Files";
+export const UCD_STAT_CHILDREN_DIRS_HEADER = "X-UCD-Stat-Children-Dirs";
--- packages/env/src/index.ts
+++ packages/env/src/index.ts
@@ -1,3 +1,10 @@
-export { UCD_STAT_TYPE_HEADER } from "./constants";
+export {
+  UCD_STAT_CHILDREN_DIRS_HEADER,
+  UCD_STAT_CHILDREN_FILES_HEADER,
+  UCD_STAT_CHILDREN_HEADER,
+  UCD_STAT_SIZE_HEADER,
+  UCD_STAT_TYPE_HEADER,
+} from "./constants";
+
 export { DEFAULT_USER_AGENT, UCDJS_API_BASE_URL } from "./env-constants";
 export { requiredEnv } from "./required-env";
