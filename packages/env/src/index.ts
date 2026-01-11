9705ef8d40d748a61f852eb6143fa6eceb624ea7
Lucas <lucasnrgaard@gmail.com>
2026-01-11 09:02:19 +0100

    feat(env): add UCDJS_STORE_BASE_URL to environment constants

--- packages/env/src/index.ts
+++ packages/env/src/index.ts
@@ -6,5 +6,5 @@ export {
   UCD_STAT_TYPE_HEADER,
 } from "./constants";
 
-export { DEFAULT_USER_AGENT, UCDJS_API_BASE_URL } from "./env-constants";
+export { DEFAULT_USER_AGENT, UCDJS_API_BASE_URL, UCDJS_STORE_BASE_URL } from "./env-constants";
 export { requiredEnv } from "./required-env";
