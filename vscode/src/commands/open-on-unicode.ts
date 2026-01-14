8899124f9ee9388a24958489a0392ccc82cdd066
Lucas <lucasnrgaard@gmail.com>
2026-01-14 06:04:43 +0100

    feat(vscode): add new commands for remote exploration and Unicode

--- vscode/src/commands/open-on-unicode.ts
+++ vscode/src/commands/open-on-unicode.ts
@@ -2,27 +2,40 @@ import type { TreeViewNode } from "reactive-vscode";
 import type { UCDTreeItem } from "../composables/useUCDExplorer";
 import { executeCommand, useCommand } from "reactive-vscode";
 import { Uri } from "vscode";
+import { config } from "../config";
 import * as Meta from "../generated/meta";
 import { logger } from "../logger";
 
 export function useOpenOnUnicodeCommand() {
-  useCommand(Meta.commands.openOnUnicode, async (treeView: TreeViewNode) => {
-    if (treeView == null) {
+  useCommand(Meta.commands.openOnUnicode, async (treeViewOrUri: Uri | TreeViewNode) => {
+    if (treeViewOrUri == null) {
       logger.error("No entry provided to openOnUnicode command.");
       return;
     }
 
-    if (typeof treeView !== "object" || !("treeItem" in treeView)) {
+    // If we get a string that is a valid URI, and its using the ucd scheme, open it in the remote explorer
+    if (treeViewOrUri instanceof Uri) {
+      if (treeViewOrUri.scheme !== "ucd") {
+        logger.error("Invalid URI scheme provided to openInRemoteExplorer command.");
+        return;
+      }
+
+      // TODO: This would allow to traverse upwards, this should be blocked.
+      executeCommand("vscode.open", Uri.parse(`https://unicode.org/Public/${treeViewOrUri.path}`));
+      return;
+    }
+
+    if (typeof treeViewOrUri !== "object" || !("treeItem" in treeViewOrUri)) {
       logger.error("Invalid entry provided to openOnUnicode command.");
       return;
     }
 
-    if (!treeView.treeItem || !(treeView.treeItem as UCDTreeItem).__ucd) {
+    if (!treeViewOrUri.treeItem || !(treeViewOrUri.treeItem as UCDTreeItem).__ucd) {
       logger.error("Invalid entry provided to openOnUnicode command.");
       return;
     }
 
-    const ucdItem = (treeView.treeItem as UCDTreeItem).__ucd;
+    const ucdItem = (treeViewOrUri.treeItem as UCDTreeItem).__ucd;
     if (!ucdItem) {
       logger.error("UCD item is undefined or null.");
       return;
