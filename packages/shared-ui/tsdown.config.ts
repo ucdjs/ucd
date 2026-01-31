ca856c113b3f425dafa5d85185a00737fb11cd7d
Lucas <lucasnrgaard@gmail.com>
2026-01-31 09:08:18 +0100

    refactor(shared-ui): clean up tsconfig and tsdown config files

--- packages/shared-ui/tsdown.config.ts
+++ packages/shared-ui/tsdown.config.ts
@@ -12,6 +12,7 @@ export default createTsdownConfig({
     customExports(exports) {
       exports["./styles/globals.css"] = "./src/styles/globals.css";
       exports["./package.json"] = "./package.json";
+
       return exports;
     },
     packageJson: false,
