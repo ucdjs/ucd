fbd26a77006460f305c6da1de2933d2397f3d619
Lucas <lucasnrgaard@gmail.com>
2026-02-05 18:57:22 +0100

    feat(pipeline-loader): add oxc-transform dependency and enhance loader functionality
    - Updated `package.json` to include `oxc-transform` as a dependency.
    - Enhanced `loader.ts` with new functions for loading pipelines from content and finding remote pipeline files.
    - Expanded type exports in `index.ts` to include new options for remote sources.

--- packages/pipelines/pipeline-loader/src/index.ts
+++ packages/pipelines/pipeline-loader/src/index.ts
@@ -4,10 +4,27 @@ export { findPipelineFiles } from "./find";
 export {
   loadPipelineFile,
   loadPipelinesFromPaths,
+  loadPipelineFromContent,
+  findRemotePipelineFiles,
+  loadRemotePipelines,
 } from "./loader";
 
 export type {
   LoadedPipelineFile,
   LoadPipelinesOptions,
   LoadPipelinesResult,
+  LoadPipelineFromContentOptions,
+  FindRemotePipelineFilesOptions,
+  LoadRemotePipelinesOptions,
 } from "./loader";
+
+export type {
+  GitHubSource,
+  GitLabSource,
+  LocalSource,
+  PipelineSource,
+  RemoteFileList,
+} from "./remote/types";
+
+export * as github from "./remote/github";
+export * as gitlab from "./remote/gitlab";
