4aa6d1b93b62e0d161b32112b885c3c87380d0aa
Lucas <lucasnrgaard@gmail.com>
2026-02-06 18:58:01 +0100

    feat(test-utils): add pipelines module and update aliases

--- packages/test-utils/src/index.ts
+++ packages/test-utils/src/index.ts
@@ -4,3 +4,8 @@ export type {
   MockStoreConfig,
 } from "./mock-store";
 export { configure, unsafeResponse } from "./mock-store/helpers";
+
+export function encodeBase64(content: string): string {
+  // eslint-disable-next-line node/prefer-global/buffer
+  return Buffer.from(content, "utf-8").toString("base64");
+}
