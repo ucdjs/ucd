30f2458d5684ff9918f9304886504a06880fad35
Lucas <lucasnrgaard@gmail.com>
2026-02-07 12:08:31 +0100

    feat(pipeline-server): enhance ESLint configuration for imports
    Added support for client and server specific imports in the ESLint configuration. Updated `package.json` to include new import paths for server files and added rules to restrict imports based on the environment.

--- packages/pipelines/pipeline-server/eslint.config.js
+++ packages/pipelines/pipeline-server/eslint.config.js
@@ -1,8 +1,35 @@
 // @ts-check
 import { luxass } from "@luxass/eslint-config";
+import pkg from "./package.json" with { type: "json" };
+
+const clientOnlyImports = Object.keys(pkg.imports).filter((key) => !key.startsWith("#server/"));
+const serverOnlyImports = Object.keys(pkg.imports).filter((key) => key.startsWith("#server/"));
 
 export default luxass({
   type: "app",
   pnpm: true,
   react: true,
+}).append({
+  files: [
+    "./src/server/**/*.ts",
+  ],
+  rules: {
+    "no-restricted-imports": ["error", {
+      patterns: [
+        ...clientOnlyImports,
+      ],
+    }],
+  },
+}).append({
+  files: [
+    "./src/client/**/*.ts",
+    "./src/client/**/*.tsx",
+  ],
+  rules: {
+    "no-restricted-imports": ["error", {
+      patterns: [
+        ...serverOnlyImports,
+      ],
+    }],
+  },
 });
