2bd31052e0eb44478bf72b4b2112393e6bb29f7a
Lucas <lucasnrgaard@gmail.com>
2026-02-09 06:34:29 +0100

    fix packages

--- packages/pipelines/pipeline-presets/tsdown.config.ts
+++ packages/pipelines/pipeline-presets/tsdown.config.ts
@@ -3,11 +3,5 @@ import { createTsdownConfig } from "@ucdjs-tooling/tsdown-config";
 export default createTsdownConfig({
   entry: [
     "./src/index.ts",
-    "./src/parsers/index.ts",
-    "./src/transforms/index.ts",
-    "./src/resolvers/index.ts",
-    "./src/sources/index.ts",
-    "./src/routes/index.ts",
-    "./src/pipelines/index.ts",
   ],
 });
