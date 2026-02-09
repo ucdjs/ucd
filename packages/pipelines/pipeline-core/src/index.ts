c9950395a5a2e38c51870b7271dc12e195783fc2
Lucas <lucasnrgaard@gmail.com>
2026-02-09 20:03:41 +0100

    feat(pipelines-core): copy from pipelines-v1

--- packages/pipelines/pipeline-core/src/index.ts
+++ packages/pipelines/pipeline-core/src/index.ts
@@ -1,1 +1,154 @@
-export {};
+export type {
+  DAG,
+  DAGNode,
+  DAGValidationError,
+  DAGValidationResult,
+} from "./dag";
+
+export {
+  buildDAG,
+  getExecutionLayers,
+} from "./dag";
+
+export type {
+  ExtractArtifactDependencies,
+  ExtractArtifactKeys,
+  ExtractRouteDependencies,
+  ParsedArtifactDependency,
+  ParsedDependency,
+  ParseDependencyType,
+  ParsedRouteDependency,
+  PipelineDependency,
+} from "./dependencies";
+
+export {
+  createArtifactDependency,
+  createRouteDependency,
+  isArtifactDependency,
+  isRouteDependency,
+  parseDependency,
+} from "./dependencies";
+
+export type {
+  ArtifactConsumedEvent,
+  ArtifactEndEvent,
+  ArtifactProducedEvent,
+  ArtifactStartEvent,
+  CacheHitEvent,
+  CacheMissEvent,
+  CacheStoreEvent,
+  FileFallbackEvent,
+  FileMatchedEvent,
+  FileSkippedEvent,
+  ParseEndEvent,
+  ParseStartEvent,
+  PipelineEndEvent,
+  PipelineError,
+  PipelineErrorEvent,
+  PipelineErrorScope,
+  PipelineEvent,
+  PipelineEventInput,
+  PipelineEventType,
+  PipelineGraph,
+  PipelineGraphEdge,
+  PipelineGraphEdgeType,
+  PipelineGraphNode,
+  PipelineGraphNodeType,
+  PipelineStartEvent,
+  ResolveEndEvent,
+  ResolveStartEvent,
+  VersionEndEvent,
+  VersionStartEvent,
+} from "./events";
+
+export {
+  always,
+  and,
+  byDir,
+  byExt,
+  byGlob,
+  byName,
+  byPath,
+  byProp,
+  bySource,
+  never,
+  not,
+  or,
+} from "./filters";
+
+export type {
+  FallbackRouteDefinition,
+  InferPipelineOutput,
+  InferPipelineRouteIds,
+  InferPipelineSourceIds,
+  PipelineDefinition,
+  PipelineDefinitionOptions,
+} from "./pipeline";
+
+export {
+  definePipeline,
+  getPipelineRouteIds,
+  getPipelineSourceIds,
+  isPipelineDefinition,
+} from "./pipeline";
+
+export type {
+  ArtifactDefinition,
+  InferArtifactType,
+  InferEmittedArtifactsFromRoute,
+  InferRouteDepends,
+  InferRouteEmits,
+  InferRouteId,
+  InferRouteOutput,
+  InferRoutesOutput,
+  InferRouteTransforms,
+  PipelineRouteDefinition,
+  RouteResolveContext,
+} from "./route";
+
+export { definePipelineRoute } from "./route";
+
+export type {
+  FileMetadata,
+  InferSourceId,
+  InferSourceIds,
+  PipelineSourceDefinition,
+  SourceBackend,
+  SourceFileContext,
+  StreamOptions,
+} from "./source";
+
+export {
+  definePipelineSource,
+  resolveMultipleSourceFiles,
+  resolveSourceFiles,
+} from "./source";
+
+export type {
+  ChainTransforms,
+  InferTransformInput,
+  InferTransformOutput,
+  PipelineTransformDefinition,
+  TransformContext,
+} from "./transform";
+
+export {
+  applyTransforms,
+  definePipelineTransform,
+} from "./transform";
+
+export type {
+  DefaultRange,
+  FileContext,
+  FilterContext,
+  ParseContext,
+  ParsedRow,
+  ParserFn,
+  PipelineFilter,
+  PropertyJson,
+  ResolveContext,
+  ResolvedEntry,
+  ResolverFn,
+  RouteOutput,
+  RowContext,
+} from "./types";
