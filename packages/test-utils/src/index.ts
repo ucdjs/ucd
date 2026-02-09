30cc648d18806edb810918f3716cb6ce33d32849
Lucas <lucasnrgaard@gmail.com>
2026-02-09 19:44:49 +0100

    feat(test-utils): add async iterable utilities and tests
    Introduce `collect` and `asyncFromArray` functions for handling async iterables.
    Add corresponding tests to ensure functionality and error handling.

--- packages/test-utils/src/index.ts
+++ packages/test-utils/src/index.ts
@@ -1,5 +1,11 @@
+export { asyncFromArray, collect } from "./async";
 export { mockStoreApi } from "./mock-store";
 export type {
   MockStoreConfig,
 } from "./mock-store";
 export { configure, unsafeResponse } from "./mock-store/helpers";
+
+export function encodeBase64(content: string): string {
+  // eslint-disable-next-line node/prefer-global/buffer
+  return Buffer.from(content, "utf-8").toString("base64");
+}
