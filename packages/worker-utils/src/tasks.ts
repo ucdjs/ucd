a2725c5ea3081cdd1573724c207c8d9417db3cc2
Lucas <lucasnrgaard@gmail.com>
2026-02-15 10:41:07 +0100

    chore: lint

--- packages/worker-utils/src/tasks.ts
+++ packages/worker-utils/src/tasks.ts
@@ -5,15 +5,15 @@ export const ALLOWED_STRING_ID_PATTERN = "^\\w[\\w-]*$";
 
 const ALLOWED_WORKFLOW_INSTANCE_ID_REGEX = new RegExp(ALLOWED_STRING_ID_PATTERN);
 
-export function makeManifestUploadId(version: string, now: () => number = Date.now) {
+export function makeManifestUploadId(version: string, now: () => number = Date.now): string {
   const normalizedVersion = version.replace(/\./g, "-");
   return `manifest-upload-${normalizedVersion}-${now()}`;
 }
 
-export function isValidWorkflowInstanceId(id: string) {
+export function isValidWorkflowInstanceId(id: string): boolean {
   return id.length <= MAX_WORKFLOW_INSTANCE_ID_LENGTH && ALLOWED_WORKFLOW_INSTANCE_ID_REGEX.test(id);
 }
 
-export function buildR2Key(version: string, workflowId: string) {
+export function buildR2Key(version: string, workflowId: string): string {
   return `manifest-tars/${version}/${workflowId}.tar`;
 }
