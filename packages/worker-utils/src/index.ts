cee4d49c009c1ddbdf6d48279648a79ecfbabe9c
Lucas <lucasnrgaard@gmail.com>
2026-02-16 05:44:48 +0100

    feat(worker-utils): add error and not found handlers

--- packages/worker-utils/src/index.ts
+++ packages/worker-utils/src/index.ts
@@ -1,6 +1,7 @@
 export * from "./cache";
 export * from "./environment";
 export * from "./errors";
+export * from "./handlers";
 export * from "./hostnames";
 export * from "./strict";
 export * from "./tasks";
