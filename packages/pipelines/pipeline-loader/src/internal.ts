ae202d401f5b6fb1a6f0fabd9afd11426c851b14
Lucas <lucasnrgaard@gmail.com>
2026-02-27 04:19:56 +0100

    chore: lint

--- packages/pipelines/pipeline-loader/src/internal.ts
+++ packages/pipelines/pipeline-loader/src/internal.ts
@@ -1,6 +1,6 @@
 /**
  * Internal exports for advanced use cases.
- * 
+ *
  * These are not part of the public API and may change in future versions.
  * Prefer using the main API via `loadPipelineFile` and `findPipelineFiles`.
  */
