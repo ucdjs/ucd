e3e32c155ba4b2159cb52e2b14f80462297de133
Lucas <lucasnrgaard@gmail.com>
2026-03-01 06:16:19 +0100

    kimi is not good at migrating

--- packages/pipelines/pipeline-server/src/client/routes/$sourceId/$fileId/$pipelineId/executions/index.tsx
+++
@@ -1,9 +0,0 @@
-import { fetchExecutions } from "#lib/pipeline-executions";
-import { createFileRoute } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/$sourceId/$fileId/$pipelineId/executions/")({
-  loader: async ({ params }) => {
-    const executions = await fetchExecutions(params.fileId, params.pipelineId, { limit: 50 }, params.sourceId);
-    return { executions };
-  },
-});
