3fe0784dde327b32b2f761cb58586fe7dde899d6
Lucas <lucasnrgaard@gmail.com>
2026-03-06 21:14:12 +0100

    chore(shared): inline pathe and std-env into bundle

--- packages/shared/tsdown.config.ts
+++ packages/shared/tsdown.config.ts
@@ -25,7 +25,7 @@ export default createTsdownConfig({
       "globalThis.__UCD_ENDPOINT_DEFAULT_CONFIG__": JSON.stringify(__UCD_ENDPOINT_DEFAULT_CONFIG__),
     }),
   ],
-  noExternal: [
+  inlineOnly: [
     "std-env",
     "pathe",
   ],
