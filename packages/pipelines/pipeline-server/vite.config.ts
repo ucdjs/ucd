93b0eff035ba39760c188e978a6220fe97ac4744
Lucas <lucasnrgaard@gmail.com>
2026-03-08 08:38:51 +0100

    refactor(pipeline-server): reorder import statements for clarity

--- packages/pipelines/pipeline-server/vite.config.ts
+++ packages/pipelines/pipeline-server/vite.config.ts
@@ -5,8 +5,11 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import Inspect from "vite-plugin-inspect";
 import viteTsConfigPaths from "vite-tsconfig-paths";
-import { h3DevServerPlugin } from "./build-plugins/h3-dev-server";
+
 import "@ucdjs/moonbeam/register";
+
+// eslint-disable-next-line perfectionist/sort-imports
+import { h3DevServerPlugin } from "./build-plugins/h3-dev-server";
 
 export default defineConfig((config) => {
   return {
