ec71cfb7596635cd4e273ca90d2ef7dabd1ab910
Lucas <lucasnrgaard@gmail.com>
2026-03-15 08:39:27 +0100

    refactor(utils): narrow public utility surface

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,28 +1,4 @@
-export {
-  createPathFilter,
-  DEFAULT_EXCLUDED_EXTENSIONS,
-  filterTreeStructure,
-  PRECONFIGURED_FILTERS,
-} from "@ucdjs-internal/shared";
-export type { PathFilter, PathFilterOptions } from "@ucdjs-internal/shared";
-
-export {
-  createGlobMatcher,
-  DEFAULT_PICOMATCH_OPTIONS,
-  isValidGlobPattern,
-  matchGlob,
-} from "@ucdjs-internal/shared";
-export type { GlobMatchFn, GlobMatchOptions } from "@ucdjs-internal/shared";
-
-export {
-  findFileByPath,
-  flattenFilePaths,
-  normalizePathForFiltering,
-  normalizeTreeForFiltering,
-} from "@ucdjs-internal/shared";
-
-export { createConcurrencyLimiter, isApiError, tryOr, wrapTry } from "@ucdjs-internal/shared";
-export type { OperationFailure, OperationResult, OperationSuccess } from "@ucdjs-internal/shared";
+export { isApiError } from "@ucdjs-internal/shared";
 
 export {
   getLatestDraftUnicodeVersion,
