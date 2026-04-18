5d16a1dbe7432ec37109faa316213a5f14ebe119
Lucas <lucasnrgaard@gmail.com>
2026-04-18 07:58:56 +0200

    refactor(api): remove deprecated ucd-store version route

--- apps/api/src/routes/well-known/router.ts
+++ apps/api/src/routes/well-known/router.ts
@@ -2,9 +2,7 @@ import type { HonoEnv } from "#types";
 import { OpenAPIHono } from "@hono/zod-openapi";
 import { WELL_KNOWN_ROUTER_BASE_PATH } from "../../constants";
 import { registerUcdConfigRoute } from "./ucd-config.json";
-import { registerUcdStoreVersionRoute } from "./ucd-store/$version";
 
 export const WELL_KNOWN_ROUTER = new OpenAPIHono<HonoEnv>().basePath(WELL_KNOWN_ROUTER_BASE_PATH);
 
 registerUcdConfigRoute(WELL_KNOWN_ROUTER);
-registerUcdStoreVersionRoute(WELL_KNOWN_ROUTER);
